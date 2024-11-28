3df352639de1959cb042f011ca446c15f0b69555
raymond <raymond@lifeacademy.org>
2024-11-28 15:46:52 +0800

    簡化寫法

--- chinesenumberconverter/Program.cs
+++ chinesenumberconverter/Program.cs
@@ -9,131 +9,13 @@ namespace chinesenumberconverter
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine(GetChineseNumberToInt("2千萬美元"));
-            Console.WriteLine(GetChineseNumberToInt("2千萬"));
-            Console.WriteLine(GetChineseNumberToInt("兩千萬"));
-
-            var numSets = new[] {
-                "0000", "0001", "0010", "0012",
-                "0100", "0102", "0310", "0123",
-                "1000", "1002", "1020", "1023",
-                "1200", "1203", "1230", "1234"
-            };
-            // 列出所有可能的組合
-            var count = numSets.Length;
-            var idxs = new int[3];
-            var lv = idxs.Length - 1;
-            while (true)
-            {
-                var sb = new StringBuilder();
-                for (var i = 0; i < idxs.Length; i++)
-                {
-                    sb.Append(numSets[idxs[i]]);
-                }
-                long n = long.Parse(sb.ToString());
-                var cht = ChtNumConverter.ToChtNum(n);
-                var restored = ChtNumConverter.ParseChtNum(cht);
-                Console.WriteLine($"{n,16:n0} {restored,16:n0} " +
-                    $"\x1b[{(n == restored ? "32mPASS" : "31mFAIL")}\x1b[0m {cht} ");
-                if (n != restored)
-                    throw new ApplicationException($"數字轉換錯誤 {n} vs {restored}");
-                if (idxs.All(o => o == count - 1)) break;
-                idxs[lv]++;
-                while (idxs[lv] == count)
-                {
-                    idxs[lv] = 0;
-                    lv--;
-                    if (lv < 0) break;
-                    idxs[lv]++;
-                }
-                lv = idxs.Length - 1;
-            }
+            Console.WriteLine(ChtNumConverter.ParseChtNum("2千萬美元"));
+            Console.WriteLine(ChtNumConverter.ParseChtNum("2千萬"));
+            Console.WriteLine(ChtNumConverter.ParseChtNum("兩千萬"));
+                        
             Console.ReadKey();
         }
-        public static long GetChineseNumberToInt(string s)
-        {
-            Dictionary<string, long> digit =
-                new Dictionary<string, long>()
-                { { "一", 1 },
-                  { "二", 2 },
-                  { "兩", 2 },
-                  { "三", 3 },
-                  { "四", 4 },
-                  { "五", 5 },
-                  { "六", 6 },
-                  { "七", 7 },
-                  { "八", 8 },
-                  { "九", 9 } };
-            Dictionary<string, long> word =
-                new Dictionary<string, long>()
-                { { "百", 100 },
-                { "千", 1000 },
-                { "萬", 10000 },
-                { "億", 100000000 },
-                { "兆", 1000000000000 } };
 
-            Dictionary<string, long> ten =
-                new Dictionary<string, long>()
-                { { "十", 10 } };
-            long iResult = 0;
-
-            s = s.Replace("零", "");
-            s = s.Replace("〇", "");
-            int index = 0;
-            long t_l = 0, _t_l = 0;
-            string t_s;
-
-            int[] numberGroups = Regex
-              .Matches(s, "[0-9]+") // groups of integer numbers
-              .OfType<Match>()
-              .Select(match => int.Parse(match.Value))
-              .ToArray();
-            int numberGroupCounter = 0;
-            while (s.Length > index)
-            {
-                t_s = s.Substring(index++, 1);
-                int originalDigit = 0;
-                // 數字
-                if (digit.ContainsKey(t_s))
-                {
-                    _t_l += digit[t_s];
-                }
-                else if (int.TryParse(t_s, out originalDigit))
-                {
-                    _t_l += numberGroups[numberGroupCounter];
-                    numberGroupCounter++;
-                }
-                // 十
-                else if (ten.ContainsKey(t_s))
-                {
-                    _t_l = _t_l == 0 ? 10 : _t_l * 10;
-                }
-                // 百、千、億、兆 
-                else if (word.ContainsKey(t_s))
-                {
-                    // 碰到千位則使 _t_l 與 t_l 相加乘上目前讀到的數字，
-                    // 並將輸出結果累加。
-                    if (word[t_s] > word["千"])
-                    {
-                        iResult += (t_l + _t_l) * word[t_s];
-                        t_l = 0;
-                        _t_l = 0;
-
-                        continue;
-                    }
-                    _t_l = _t_l * word[t_s];
-                    t_l += _t_l;
-
-                    _t_l = 0;
-                }
-            }
-            // 將殘餘值累加至輸出結果
-            iResult += t_l;
-            iResult += _t_l;
-
-            return iResult;
-
-        }
         public class ChtNumConverter
         {
             public static string ChtNums = "零一二三四五六七八九";
@@ -160,12 +42,24 @@ namespace chinesenumberconverter
                 {
                     long lastDigit = 0;
                     long subNum = 0;
+                    int numberGroupCounter = 0;
+                    int originalDigit = 0;
+                    int[] numberGroups = Regex
+                    .Matches(s, "[0-9]+") // groups of integer numbers
+                    .OfType<Match>()
+                    .Select(match => int.Parse(match.Value))
+                    .ToArray();
                     foreach (var rawChar in s)
                     {
-                        var c = rawChar.ToString().Replace("〇", "零");
+                        var c = rawChar.ToString().Replace("〇", "零").Replace("兩", "二");
                         if (ChtNums.Contains(c))
                         {
                             lastDigit = (long)ChtNums.IndexOf(c);
+                        }
+                        else if (int.TryParse(c, out originalDigit))
+                        {
+                            lastDigit = numberGroups[numberGroupCounter];
+                            numberGroupCounter++;
                         }
                         else if (ChtUnits.ContainsKey(c))
                         {
@@ -174,10 +68,10 @@ namespace chinesenumberconverter
                             subNum += lastDigit * unit;
                             lastDigit = 0;
                         }
-                        else
-                        {
-                            throw new ArgumentException($"包含無法解析的中文數字：{c}");
-                        }
+                        //else
+                        //{
+                        //    throw new ArgumentException($"包含無法解析的中文數字：{c}");
+                        //}
                     }
                     subNum += lastDigit;
                     return subNum;
--- chinesenumberconverter/obj/Debug/net8.0/chinesenumberconverter.AssemblyInfo.cs
+++ chinesenumberconverter/obj/Debug/net8.0/chinesenumberconverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chinesenumberconverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b86dd38bb50b51fc23133d21552603ca4944a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5411e981d9a805cc94bba9c9acf83056e7d6f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("chinesenumberconverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chinesenumberconverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
